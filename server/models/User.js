0fae00b6709efee57df63103d11ea16d3a94cfa3
Scrysis <Scrysis@users.noreply.github.com>
2024-03-17 21:09:49 -0700

    Preliminary models

---
+++ server/models/User.js
@@ -0,0 +1,49 @@
+const { Schema, Model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  /*  advice for validating email found: https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax */
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+      },
+      message: "Please enter a valid email",
+    },
+    required: [true, "Email required"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: [8],
+  },
+  birbArray: [Number],
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model("user", userSchema);
+
+module.exports = User;
