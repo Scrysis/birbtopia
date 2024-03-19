74bbf077f269a064c994200f20abdd8ebc997601
jessclark1234 <jessicaclark7890@gmail.com>
2024-03-18 21:25:56 -0700

    saving changes

--- server/models/User.js
+++ server/models/User.js
@@ -44,6 +44,6 @@ userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-const User = model("user", userSchema);
+const User = Model("User", userSchema);
 
 module.exports = User;
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,33 @@
+const { User } = require('../models');
+const { jwtToken, authError } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        profile: async () => {
+            return User.find();
+        },
+
+        profile: async (_parent, { userId }) => {
+            return User.findOne({ _id: userId});
+        },
+    },
+    Mutation: {
+        addUser: async (parent, {name, email, password}) => {
+            const user = await User.create({ name, email, password });
+            const token = jwtToken(user);
+
+            return { token, user };
+        },
+        login: async (_, { email, password }) => {
+            const user = await User.findOne({ _id: userId });
+
+            if (!userId) {
+                throw authError
+            }
+
+            const correctPassword = await user.isCorrectPassword(password);
+
+            if (!correctPassword)
+        }
+    }
+}
