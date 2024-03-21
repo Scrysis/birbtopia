c40d18b16c5051cc24d797d17df509a5f241fc73
jessclark1234 <jessicaclark7890@gmail.com>
2024-03-20 21:18:32 -0700

    Partone

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,36 +2,56 @@ const { User } = require('../models');
 const { jwtToken, authError } = require('../utils/auth');
 
 const resolvers = {
-    Query: {
-        profile: async () => {
-            return User.find();
-        },
-
-        profile: async (_parent, { userId }) => {
-            return User.findOne({ _id: userId});
-        },
+  Query: {
+    profiles: async () => {
+      return User.find();
     },
-    Mutation: {
-        addUser: async (parent, {name, email, password}) => {
-            const user = await User.create({ name, email, password });
-            const token = jwtToken(user);
 
-            return { token, user };
+    profile: async (_parent, { profileId }) => {
+      return User.findOne({ _id: profileId });
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { name, email, password }) => {
+      const profile = await User.create({ name, email, password });
+      const token = jwtToken(username);
+
+      return { token, profile };
+    },
+    login: async (_, { email, password }) => {
+      const profile = await User.findOne({ email });
+
+      if (!profile) {
+        throw authError
+      }
+
+      const correctPw = await profile.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw authError
+      }
+
+      const token = jwtToken(profile);
+      return { token, profile };
+    },
+
+    addSkill: async (parent, { profileId, skill }) => {
+      return User.findOneAndUpdate(
+        { _id: profileId },
+        {
+          $addToSet: { skills: skill },
         },
-        login: async (_, { email, password }) => {
-            const user = await User.findOne({ _id: userId });
-
-            if (!userId) {
-                throw authError
-            }
-
-            const correctPassword = await user.isCorrectPassword(password);
-
-            if (!correctPassword) {
-                throw authError
-            }
-
-            const jwt = jwtToken(email)
+        {
+          new: true,
+          runValidators: true,
         }
-    }
-}
+      );
+    },
+    removeUser: async (parent, { profileId }) => {
+      return User.findOneAndDelete({ _id: profileId });
+    },
+  },
+};
+
+module.exports = resolvers;
