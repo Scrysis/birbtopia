10ef7dc91f307d996222372f9ca20bfbaadb03e9
Garrett Gonzales <gggonzo4@yahoo.com>
2024-03-23 19:27:50 -0700

    added more homepage css

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,6 +1,6 @@
 import "./home.css";
 import Logo from "../assets/birbtopiaLogo.png";
-import Sky from "../assets/cloud-sky-cartoon-background-blue-sky-with-white-clouds-flat-poster-or-flyer-cloudscape-panorama-pattern-seamless-colored-abstract-fluffy-texture-vector.jpg";
+// import Sky from "../assets/cloud-sky-cartoon-background-blue-sky-with-white-clouds-flat-poster-or-flyer-cloudscape-panorama-pattern-seamless-colored-abstract-fluffy-texture-vector.jpg";
 
 const Home = () => {
   return (
@@ -14,12 +14,10 @@ const Home = () => {
               Welcome to Birbs, feel free to look around, get Birbs and feed
               them! If you like the game feel free to support our developers!
             </div>
+            <div className="usersBirbs">Its all of Users Birbs!</div>
           </div>
         </div>
       </div>
-      <div className="lineBreak"></div>
-      <img src={Sky} alt="Sky" className="SkyBackground" />
-      <div className="Footer"> </div>
     </div>
   );
 };
