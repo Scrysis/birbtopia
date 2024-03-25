2a5989cda4296760f6ede314577032499cef90a2
jessclark1234 <jessicaclark7890@gmail.com>
2024-03-24 20:11:50 -0700

    fixed App.jsx

---
+++ client/src/components/pages/Home.jsx
@@ -0,0 +1,24 @@
+import "./home.css";
+import Logo from "../assets/birbtopiaLogo.png";
+// import Sky from "../assets/cloud-sky-cartoon-background-blue-sky-with-white-clouds-flat-poster-or-flyer-cloudscape-panorama-pattern-seamless-colored-abstract-fluffy-texture-vector.jpg";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="homePage">
+        <img src={Logo} alt="Logo" className="logo" />
+        <div className="homePageHeader">
+         Hello, User!
+          <div className="newContainer">
+            <div className="subHomePageHeader">
+              Welcome to *:･ﾟ✧ Birbtopia ✧･ﾟ:*! feel free to look around, get Birbs and feed
+              them! If you like the game feel free to support our developers!
+            </div>
+            <div className="usersBirbs">Its all of Users Birbs!</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Home;
