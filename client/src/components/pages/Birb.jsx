7bc79befc03e5b7f0cf7a761b6e440f31f968b05
Garrett Gonzales <gggonzo4@yahoo.com>
2024-03-25 19:16:47 -0700

    re adding code

--- client/src/components/pages/Birb.jsx
+++ client/src/components/pages/Birb.jsx
@@ -1,40 +1,41 @@
-import { useQuery} from '@apollo/client';
-import { useParams} from 'react-router-dom';
+import { useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
 
-import { QUERY_SINGLE_BIRB } from '../utils/queries';
+import { QUERY_SINGLE_BIRB } from "../../utils/queries";
 
 const SingleBirb = () => {
-    const { birbId} = useParams();
+  const { birbId } = useParams();
 
-    const { loading, data} = useQuery(QUERY_SINGLE_BIRB, {
-        variables: { birbId: birbId},
-    });
+  const { loading, data } = useQuery(QUERY_SINGLE_BIRB, {
+    variables: { birbId: birbId },
+  });
 
-    const birb = data?.Birb || {};
+  const birb = data?.Birb || {};
 
-    if (loading) {
-        return <div>Getting Birb . . .</div>;
-    }
+  if (loading) {
+    return <div>Getting Birb . . .</div>;
+  }
 
-    return(
-        <div style={{
-            display: 'flex',
-            flexDirection: 'row',
-        }}>
-            <div>
-                <ul>
-                    <li>Birb Name: {birb.birbname}</li>
-                    <li>Birb Food: {birb.food}</li>
-                    <li>Birb Happiness: {birb.happiness}</li>
-                    <li>Birb Health: {birb.health}</li>
-                    <li>Birb Magic: {birb.magic}</li>
-                    <li>Birb Stamina: {birb.stamina}</li>
-                
-                </ul>
-            </div>
-            {birb.appearance.birbAppearance}
-        </div>
-    );
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+      }}
+    >
+      <div>
+        <ul>
+          <li>Birb Name: {birb.birbname}</li>
+          <li>Birb Food: {birb.food}</li>
+          <li>Birb Happiness: {birb.happiness}</li>
+          <li>Birb Health: {birb.health}</li>
+          <li>Birb Magic: {birb.magic}</li>
+          <li>Birb Stamina: {birb.stamina}</li>
+        </ul>
+      </div>
+      {birb.appearance.birbAppearance}
+    </div>
+  );
 };
 
 export default SingleBirb;
