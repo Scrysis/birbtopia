47bae8dfa8759a82ab4c4cce03e2f8b6a5b8ac47
tor first <victoriah.first@gmail.com>
2024-03-25 23:09:36 -0500

    update profile css

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -11,7 +11,7 @@ export default function Profile() {
                 <div className="profileSection">
                     <div className="userProfile">
                         <h1>User Bio</h1>
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra nam libero justo laoreet sit amet. Amet est placerat in egestas erat imperdiet sed. Suspendisse faucibus interdum posuere lorem. Vitae tempus quam pellentesque nec nam aliquam. Ultricies leo integer malesuada nunc vel risus. Sem fringilla ut morbi tincidunt augue interdum velit euismod in. Magna eget est lorem ipsum dolor sit amet. Adipiscing enim eu turpis egestas pretium aenean pharetra. Vel turpis nunc eget lorem dolor. Interdum consectetur libero id faucibus nisl tincidunt eget nullam. Enim diam vulputate ut pharetra sit amet aliquam id diam.</div>
+                        <div><h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra nam libero justo laoreet sit amet. Amet est placerat in egestas erat imperdiet sed. Suspendisse faucibus interdum posuere lorem. Vitae tempus quam pellentesque nec nam aliquam. Ultricies leo integer malesuada nunc vel risus. Sem fringilla ut morbi tincidunt augue interdum velit euismod in. Magna eget est lorem ipsum dolor sit amet. Adipiscing enim eu turpis egestas pretium aenean pharetra. Vel turpis nunc eget lorem dolor. Interdum consectetur libero id faucibus nisl tincidunt eget nullam. Enim diam vulputate ut pharetra sit amet aliquam id diam.</h3></div>
                 {/* <button>Edit Profile</button>
                 <button>Delete Account</button>  */}
 
