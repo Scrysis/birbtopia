206ebf68054f404d5626da8606489dbf00d4659c
Scrysis <Scrysis@users.noreply.github.com>
2024-03-25 18:33:25 -0700

    birb stuff

--- client/src/pages/birb.jsx
+++ client/src/pages/birb.jsx
@@ -1,14 +1,40 @@
-import { useQuery, useMutation } from '@apollo/client';
-import { useParams, Link } from 'react-router-dom';
+import { useQuery} from '@apollo/client';
+import { useParams} from 'react-router-dom';
 
-const birbCard = () => {
+import { QUERY_SINGLE_BIRB } from '../utils/queries';
 
-    return (
-        <div className="mainBirbCard">
-            {/* birbCardPic div must contain the birb picture and only the birb picture. */}
-            <div className="birbCardPic">
+const SingleBirb = () => {
+    const { birbId} = useParams();
 
+    const { loading, data} = useQuery(QUERY_SINGLE_BIRB, {
+        variables: { birbId: birbId},
+    });
+
+    const birb = data?.Birb || {};
+
+    if (loading) {
+        return <div>Getting Birb . . .</div>;
+    }
+
+    return(
+        <div style={{
+            display: 'flex',
+            flexDirection: 'row',
+        }}>
+            <div>
+                <ul>
+                    <li>Birb Name: {birb.birbname}</li>
+                    <li>Birb Food: {birb.food}</li>
+                    <li>Birb Happiness: {birb.happiness}</li>
+                    <li>Birb Health: {birb.health}</li>
+                    <li>Birb Magic: {birb.magic}</li>
+                    <li>Birb Stamina: {birb.stamina}</li>
+                
+                </ul>
             </div>
+            {birb.appearance.birbAppearance}
         </div>
-    )
-}
+    );
+};
+
+export default SingleBirb;
